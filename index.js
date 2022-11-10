b1d65353f136251aa44f00710d9c132c28ae0592
Fanie Ndlovu <faniejohnsons98@gmail.com>
2022-11-10 16:24:46 +0200

    api

---
+++ index.js
@@ -0,0 +1,32 @@
+const userTemplateText = document.querySelector('.myTemplate');
+const userTemplate = Handlebars.compile(userTemplateText.innerText);
+
+const usersElem = document.querySelector('.users');
+const colorElem = document.querySelector('.colors');
+
+
+axios
+    .get(`http://api-tutor.herokuapp.com/v1/cars`)
+        .then(result => {
+            const posts = result.data;
+            
+            let list = userTemplate({
+                cars : posts  
+            })
+            usersElem.innerHTML = list
+    })
+
+axios 
+    .get(`http://api-tutor.herokuapp.com/v1/colors`)
+    .then( apiData => {
+        const colors = apiData.data;
+        let list = userTemplate({
+            color : colors
+        })
+        colorElem.innerHTML = list;
+
+    }
+
+    )
+
+
